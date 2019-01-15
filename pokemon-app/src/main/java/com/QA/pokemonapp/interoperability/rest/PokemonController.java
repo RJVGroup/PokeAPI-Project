05768398da33bedfe016fb77b81705f0c6201c30
vincent Yeadon <v.yeadon@hotmail.co.uk>
2019-01-15 16:54:13 +0000

    Added in different layers for the pokemon class and created pokemon generator

--- pokemon-app/src/main/java/com/QA/pokemonapp/interoperability/rest/PokemonController.java
+++ pokemon-app/src/main/java/com/QA/pokemonapp/interoperability/rest/PokemonController.java
@@ -1,25 +1,41 @@
 package com.QA.pokemonapp.interoperability.rest;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.QA.pokemonapp.persistance.domain.Pokemon;
-import com.QA.pokemonapp.persistance.repository.PokemonRepository;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("api/v2")
 public class PokemonController {
 	
 	@Autowired
-	private PokemonRepository pokemonRepository;
+	private RestTemplate restTemplate;
 	
-	@GetMapping("/pokemon/{nameOfPokemon}")
-	public Pokemon getPokemon(@PathVariable String Pokemon)
+	public String getPokemonJsonString(String pokemon)
 	{
-		return null;
-		
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity <String> entity = new HttpEntity<String>(headers);
+	    
+		return restTemplate.exchange(
+	    		"https://pokeapi.co/pokemon/" + pokemon, HttpMethod.GET, entity, String.class).getBody();
+	}
+	
+
+	public String getPokemonSpeciesJsonString(@PathVariable String pokemon)
+	{
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity <String> entity = new HttpEntity<String>(headers);
+	    
+	    return restTemplate.exchange(
+	    		"https://pokeapi.co/pokemon-species/" + pokemon, HttpMethod.GET, entity, String.class).getBody();
 	}
 }
