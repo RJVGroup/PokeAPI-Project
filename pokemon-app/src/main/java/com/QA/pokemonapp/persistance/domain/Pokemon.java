b50f980d6956a0a27747bb849c6dd94fb5766394
vincent Yeadon <v.yeadon@hotmail.co.uk>
2019-01-15 11:25:41 +0000

    Started adding auto doc comments and added level up methods

--- pokemon-app/src/main/java/com/QA/pokemonapp/persistance/domain/Pokemon.java
+++ pokemon-app/src/main/java/com/QA/pokemonapp/persistance/domain/Pokemon.java
@@ -1,3 +1,13 @@
+/**
+* <h1>Pokemon Class<h1>
+* This pokemon class contains the variables and methods which will be used 
+* for a pokemon object
+*
+* @author  Vincent Yeadon
+* @version 1.0
+* @since   2019-01-15 
+*/
+
 package com.QA.pokemonapp.persistance.domain;
 
 import java.util.ArrayList;
@@ -14,18 +24,38 @@ public class Pokemon {
 	private int XP;
 	private int level;
 	private int currentHP;
-	
-	// HP, Attack, Defence, Sp.Atk, Sp.Def, Speed
+
+	/**
+   * The following three arrays contain stat information for each pokemon
+   * in the following format
+   * @param statList[0] This is the HP
+   * @param statList[1] This is the Attack
+   * @param statList[2] This is the Defence
+   * @param statList[3] This is the Special Attack
+   * @param statList[4] This is the Special Defence
+   * @param statList[5] This is the Speed
+   */
 	private int[] statList = new int[5];
 	private int[] baseStat = new int[5];
 	private int[] IV = new int[5];
 	
 	private Set<Move> moveList;
 	
+	public void checkIfLevelUp(int currentLevel, int XP)
+	{
+		if(XP >= Math.pow((currentLevel + 1), 3))
+		{
+			levelUp();
+			this.level += 1;
+		}
+	}
 	
 	public void levelUp()
 	{
-
+		for(int i=0; i<6; i++)
+		{
+			this.statList[i] = this.statList[i]*(this.baseStat[i]/50 + this.IV[i]/100);
+		}
 	}
 	
 	public void useItem(String item)
@@ -113,16 +143,7 @@ public class Pokemon {
 		this.level = level;
 	}
 
-	public int getIV() {
-		return IV;
-	}
-
-
-	public List<Integer> getStatList() {
-		return statList;
-	}
-
-	public void setStatList(List<Integer> statList) {
+	public void setStatList(int[] statList) {
 		this.statList = statList;
 	}
 
