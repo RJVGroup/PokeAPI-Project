f6f1e2580690c06978633e3f0addcbc85b15b3fa
ME <ME@GIB-GOLDEN-LEGENDARY>
2019-01-17 11:44:05 +0000

    Added class which describes the damage relations between Pokemon Types (to be used with data from the api)

--- pokemon-app/src/main/java/com/QA/pokemonapp/constantsandenums/TypeEffectivenessChecker.java
+++ pokemon-app/src/main/java/com/QA/pokemonapp/constantsandenums/TypeEffectivenessChecker.java
@@ -6,11 +6,11 @@ public class TypeEffectivenessChecker {
 	public int returnDamageModifier(ETypes attackingType, List<ETypes> defendingType) {
 		int damageMultiplier = 1;
 		for (ETypes type : defendingType) {
-			if(APIManager.getTypeInfo(type).doubleDamage.contains(attackingType.name().toLowerCase())) {
+			if(APIManager.getTypeInfo(type).doubleDamage().contains(attackingType)) {
 				damageMultiplier *= 2;
-			} else if(APIManager.getTypeInfo(type).halfDamage.contains(attackingType.name().toLowerCase())) {
+			} else if(APIManager.getTypeInfo(type).halfDamage().contains(attackingType)) {
 				damageMultiplier /= 2;
-			} else if(APIManager.getTypeInfo(type).noDamage.contains(attackingType.name().toLowerCase())) {
+			} else if(APIManager.getTypeInfo(type).noDamage().contains(attackingType)) {
 				damageMultiplier *= 0;
 			}
 		}
