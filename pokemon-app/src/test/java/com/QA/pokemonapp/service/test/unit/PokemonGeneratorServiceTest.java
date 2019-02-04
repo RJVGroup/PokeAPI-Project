9402d8c8f603a0048a088511a8e94c2ef9e337cd
vincent Yeadon <v.yeadon@hotmail.co.uk>
2019-02-04 16:28:33 +0000

    more tests

--- pokemon-app/src/test/java/com/QA/pokemonapp/service/test/unit/PokemonGeneratorServiceTest.java
+++ pokemon-app/src/test/java/com/QA/pokemonapp/service/test/unit/PokemonGeneratorServiceTest.java
@@ -3,8 +3,8 @@ package com.QA.pokemonapp.service.test.unit;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,7 +16,6 @@ import com.QA.pokemonapp.business.service.PokemonGeneratorService;
 import com.QA.pokemonapp.business.service.move.MoveInterface;
 import com.QA.pokemonapp.interoperability.rest.PokemonPokeAPIController;
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 
@@ -39,8 +38,8 @@ public class PokemonGeneratorServiceTest {
         MockitoAnnotations.initMocks(this);
 	
         Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader("C:\\Users\\bbtvi\\Documents\\#Work_LYF\\QA\\Pokemon\\pokemon-app\\src\\test\\java\\mockPokemon.json"));
-        String data = gson.fromJson(reader, String.class);
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\bbtvi\\Documents\\#Work_LYF\\QA\\Pokemon\\pokemon-app\\src\\test\\java\\mockPokemon.txt");
+        String data = inputStream.toString();
         
 		pokemonJson = Configuration.defaultConfiguration().jsonProvider().parse(data);
 		
